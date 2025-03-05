87d66b787fc22409b3ce0cfdec4db72b6b25f3c5
FirstGo1 <figoaja313@gmail.com>
2025-03-05 13:15:28 +0700

    first upload

---
+++ galeri/script.js
@@ -0,0 +1,94 @@
+// -------JavaScript for toggle Menu------
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+  navLinks.style.right = "0";
+}
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
+
+// ----------------- GALERI HTML ----------
+const container = document.querySelector(".myGallery");
+const likeBtns = document.querySelectorAll(".like-btn");
+
+var swiper = new Swiper(".myGallery .swiper", {
+  spaceBetween: 30,
+  slidesPerView: "auto",
+  loop: true,
+  speed: 2500,
+  freeMode: true,
+  allowTouchMove: true,
+  lazyLoading: true,
+  autoplay: {
+    delay: 0,
+  },
+});
+
+function stopAutoplay() {
+  const swiperTranslate = swiper.getTranslate();
+  swiper.setTranslate(swiperTranslate);
+  swiper.autoplay.stop();
+}
+
+function startAutoplay() {
+  swiper.slideTo(swiper.activeIndex, 1000, false);
+  swiper.autoplay.start();
+}
+
+container.addEventListener("mouseenter", () => stopAutoplay());
+container.addEventListener("mouseleave", () => startAutoplay());
+
+likeBtns.forEach((likeBtn) => {
+  likeBtn.addEventListener("click", () => {
+    likeBtn.classList.toggle("active");
+    likeBtn.classList.toggle("pulse");
+  });
+});
+
+
+
+
+
+
+// ---------- galeri 1 ----------- 
+// ---------- galeri --------- 
+var index = new Swiper("#swiper-2", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 17,
+  autoplay: {
+    delay: 2000,
+  },
+  lazyLoading: true,
+  loop: true,
+  keyboard: {
+    enabled: true,
+  },
+  navigation: {
+    nextEl: "#nav-right",
+    prevEl: "#nav-left",
+  },
+  breakpoints: {
+    800: {
+      slidesPerView: 3,
+    },
+    1400: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+// ------------ onclick zoom img --------- 
+
+document.querySelectorAll("#swiper-2 .swiper-slide img").forEach((image) => {
+  image.onclick = () => {
+    document.querySelector(".popup-image").style.display = "block";
+    document.querySelector(".popup-image img").src = image.getAttribute("src");
+  };
+});
+
+document.querySelector(".popup-image span").onclick = () => {
+  document.querySelector(".popup-image").style.display = "none";
+};
--- javascript.js
+++ javascript.js
@@ -1,54 +1,18 @@
-//menu
-var tombolMenu = $(".tombol-menu");
-var menu = $("nav .menu ul");
 
-function klikMenu() {
-  tombolMenu.click(function () {
-    menu.toggle();
-  });
-  menu.click(function () {
-    menu.toggle();
-  });
+// -------JavaScript for toggle Menu------
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+  navLinks.style.right = "0";
+}
+function hideMenu() {
+  navLinks.style.right = "-200px";
 }
 
-$(document).ready(function () {
-  var width = $(window).width();
-  if (width < 990) {
-    klikMenu();
-  }
-});
-
-//check lebar
-$(window).resize(function () {
-  var width = $(window).width();
-  if (width > 989) {
-    menu.css("display", "block");
-    //display:block
-  } else {
-    menu.css("display", "none");
-  }
-  klikMenu();
-});
-
-//efek scroll
-$(document).ready(function () {
-  var scroll_pos = 0;
-  $(document).scroll(function () {
-    scroll_pos = $(this).scrollTop();
-    if (scroll_pos > 0) {
-      $("nav").addClass("putih");
-      $("nav img.hitam").show();
-      $("nav img.putih").hide();
-    } else {
-      $("nav").removeClass("putih");
-      $("nav img.hitam").hide();
-      $("nav img.putih").show();
-    }
-  });
-});
-
 //  Swiper for  tampilan depan
-const swiperTampilanDepan = new Swiper(".swiper-tampilan-depan", {
+let swiperTampilanDepan = new Swiper(".swiper-tampilan-depan", {
+  loop: true,
   // effect: 'fade',
   autoplay: {
     delay: 5000,
@@ -66,11 +30,38 @@ const swiperTampilanDepan = new Swiper(".swiper-tampilan-depan", {
   },
 });
 
+// ---------- galeri --------- 
+var index = new Swiper("#swiper-2", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 17,
+  autoplay: {
+    delay: 2000,
+  },
+  lazyLoading: true,
+  loop: true,
+  keyboard: {
+    enabled: true,
+  },
+  navigation: {
+    nextEl: "#nav-right",
+    prevEl: "#nav-left",
+  },
+  breakpoints: {
+    800: {
+      slidesPerView: 3,
+    },
+    1400: {
+      slidesPerView: 5,
+    },
+  },
+});
 
 
 
-//  Pop up image onclick
-document.querySelectorAll("#gallery img").forEach((image) => {
+
+//  ONCLICK zoom
+document.querySelectorAll("#swiper-2 .swiper-slide img").forEach((image) => {
   image.onclick = () => {
     document.querySelector(".popup-image").style.display = "block";
     document.querySelector(".popup-image img").src = image.getAttribute("src");
@@ -82,39 +73,42 @@ document.querySelector(".popup-image span").onclick = () => {
 };
 
 
-// SCRIPT FOR SLIDER
-new Swiper(".card-wrapper", {
+// ----------------- GALERI HTML ---------- 
+const container = document.querySelector(".rows")
+const likeBtns = document.querySelector(".like-btn")
+
+var swiper = new Swiper(".rows .swiper", {
+  spaceBetween: 30,
+  slidesPerView: "auto",
   loop: true,
-  spaceBetween: 0,
+  speed: 2500,
+  freeMode: true,
+  allowTouchMove: true,
+  lazyLoading: true,
   autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,  
-  },
-
-  // pagination bullets
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // Responsive breakpoints
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 4,
-    },
-
+    delay: 0,
   },
 });
+
+// function stopAutoplay() {
+//   const swiperTranslate = swiper.getTranslate();
+//   swiper.setTranslate(swiperTranslate);
+//   swiper.autoplay.stop();
+// }
+
+// function startAutoplay() {
+//   swiper.slideTo(swiper.activeIndex, 3000, false);
+//   swiper.autoplay.start();
+// }
+
+// container.addEventListener("mouseenter", () => stopAutoplay());
+// container.addEventListener("mouseleave", () => startAutoplay());
+
+// likeBtns.forEach((likeBtn) => {
+//   likeBtn.addEventListener("click", () => {
+//     likeBtn.classList.toggle("active");
+//     likeBtn.classList.toggle("pulse");
+//   });
+// });
+
+
