b3db22322f8e21a7271a9ac247baed7cb6dbc0a3
FirstGo1 <figoaja313@gmail.com>
2025-03-20 12:18:12 +0700

    popup ketentuan

--- tes/script.js
+++ tes/script.js
@@ -1,39 +1,36 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex',
-    sidebar.classList.add('show')
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  (sidebar.style.display = "flex"), sidebar.classList.add("show");
 }
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
 }
 
-// --------------- scroll ------------- 
-window.addEventListener('scroll', function() {
-    var navBawah = document.querySelector('.nav-bawah');
-    var scrollPosition = window.scrollY;
-    if (scrollPosition > 200) {
-      navBawah.classList.add('show');
-    } else {
-      navBawah.classList.remove('show');
-    }
-  });
+// --------------- scroll -------------
+window.addEventListener("scroll", function () {
+  var navBawah = document.querySelector(".nav-bawah");
+  var scrollPosition = window.scrollY;
+  if (scrollPosition > 200) {
+    navBawah.classList.add("show");
+  } else {
+    navBawah.classList.remove("show");
+  }
+});
 
-
-
-// ---------- bagian form------------ 
-const form = document.querySelector('form');
-const inputs = form.querySelectorAll('input[required], select[required]');
-const button = form.querySelector('.formbold-btn');
+// ---------- bagian form------------
+const form = document.querySelector("form");
+const inputs = form.querySelectorAll("input[required], select[required]");
+const button = form.querySelector(".formbold-btn");
 
 inputs.forEach((input) => {
-  input.addEventListener('input', () => {
-    const allFilled = Array.from(inputs).every((input) => input.value !== '');
+  input.addEventListener("input", () => {
+    const allFilled = Array.from(inputs).every((input) => input.value !== "");
     if (allFilled) {
-      button.classList.remove('disabled');
+      button.classList.remove("disabled");
       button.disabled = false;
     } else {
-      button.classList.add('disabled');
+      button.classList.add("disabled");
       button.disabled = true;
     }
   });
@@ -41,38 +38,63 @@ inputs.forEach((input) => {
 
 
 
+// ------------ popup syarat 2------------
+const formboldBtn = document.querySelector(".formbold-btn");
+const termsBox = document.querySelector(".terms-box");
+const termsBoxClose = document.querySelector(".terms-box-close");
+const formboldPolicyCheckbox = document.querySelector('#syarat-ketentuan');
 
-// ---------- pop-up syarat ----------
-const syaratKetentuanLink = document.querySelector('.syarat-ketentuan-link');
-const syaratKetentuanPopup = document.querySelector('.syarat-ketentuan-popup');
-const syaratKetentuanPopupClose = document.querySelector('.syarat-ketentuan-popup-close');
-
-syaratKetentuanLink.addEventListener('click', () => {
-  syaratKetentuanPopup.style.display = 'block';
+formboldPolicyCheckbox.addEventListener('change', () => {
+  if (formboldPolicyCheckbox.checked) {
+    termsBox.classList.add('show');
+  } else {
+    termsBox.classList.remove('show');
+  }
 });
 
-syaratKetentuanPopupClose.addEventListener('click', () => {
-  syaratKetentuanPopup.style.display = 'none';
+// formboldBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   termsBox.classList.add("show");
+// });
+
+termsBoxClose.addEventListener("click", () => {
+  termsBox.classList.remove("show");
 });
 
-syaratKetentuanLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    syaratKetentuanPopup.style.display = 'block';
-});   
-
-
-const riwayatKesehatanSelect = document.getElementById('riwayat_kesehatan');
-const riwayatKesehatanLainnyaInput = document.getElementById('riwayat_kesehatan_lainnya');
-
-riwayatKesehatanSelect.addEventListener('change', () => {
-    if (riwayatKesehatanSelect.value === 'Lainnya') {
-      riwayatKesehatanSelect.style.display = 'none';
-      riwayatKesehatanLainnyaInput.style.display = 'block';
+// ----- disabled button accept ---
+document
+  .getElementById("terms-checkbox")
+  .addEventListener("change", function () {
+    if (this.checked) {
+      document.getElementById("accept-btn").disabled = false;
     } else {
-      riwayatKesehatanSelect.style.display = 'block';
-      riwayatKesehatanLainnyaInput.style.display = 'none';
+      document.getElementById("accept-btn").disabled = true;
     }
   });
 
+// ----------accept & decline button close popup-----------
+const declineBtn = document.querySelector(".gray-btn");
+declineBtn.addEventListener("click", () => {
+  termsBox.classList.remove("show");
+});
+const acceptBtn = document.querySelector(".red-btn");
+acceptBtn.addEventListener("click", () => {
+  termsBox.classList.remove("show");
+});
 
 
+// --------------- form riwayat kesehatan = pilihan lainnya ----------
+const riwayatKesehatanSelect = document.getElementById("riwayat_kesehatan");
+const riwayatKesehatanLainnyaInput = document.getElementById(
+  "riwayat_kesehatan_lainnya"
+);
+
+riwayatKesehatanSelect.addEventListener("change", () => {
+  if (riwayatKesehatanSelect.value === "Lainnya") {
+    riwayatKesehatanSelect.style.display = "none";
+    riwayatKesehatanLainnyaInput.style.display = "block";
+  } else {
+    riwayatKesehatanSelect.style.display = "block";
+    riwayatKesehatanLainnyaInput.style.display = "none";
+  }
+});
